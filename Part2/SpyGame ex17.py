b7d5d75247897bc240a11aec18b24218649f3b86
Reez32 <reezdyhar327@gmail.com>
2025-09-29 08:30:30 +0000

    Part2 Reez

---
+++ Part2/SpyGame ex17.py
@@ -0,0 +1,7 @@
+print("What is your name, bigbro?")
+name = input()
+import random
+adjective = ['Black', 'Fire', 'Bodacious', 'Fearless', 'Lucky', 'BigBrain']
+animal = ['Dragon', 'Tiger', 'Panther', 'Jaguar', 'Lion', 'Griffin']
+print(f"{name} is such an L name, I'll call you", random.choice(adjective), random.choice(animal), "instead")
+print("Your number of sclerosis are", random.randrange(0, 100))
---
+++ Part2/ex13.py
@@ -0,0 +1,14 @@
+# Start from zero
+count = 0
+
+# TODO:
+# Create a while loop that prints the count
+# Stop the loop when count reaches 5
+
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+    if i == 6:
+        break
+# Don't forget to increase count each loop!
